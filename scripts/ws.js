cd624f23fe9d629f4f18ce28e898e1df2c6e7e9e
Ryan Moeller <rmoeller.dev@gmail.com>
2025-09-20 19:21:20 -0400

    scripts/ws: Use Bulma native delete button instead of Font Awesome
    Remove the trash icon and instead use the Bulma delete class.  Instead
    of disabling the button for the active BE, only create the button if the
    BE can be deleted.

--- scripts/ws.js
+++ scripts/ws.js
@@ -14,8 +14,6 @@ const CMD_SNAP_DELETE = "snap_delete";
 /* TODO: move this into the server configuration */
 const basedir = "/system";
 
-const trashIconHTML = '<svg xmlns="http://www.w3.org/2000/svg" class="icon" aria-hidden="true" focusable="false" viewBox="0 0 448 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path fill="currentColor" d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"/></svg>';
-
 let g_bootenvs = [];
 
 function be_find(name) {
@@ -76,18 +74,15 @@ const handlers = new Map([
             const creation = row.insertCell();
             creation.innerText = format_timestamp(be.creation);
             const del = row.insertCell();
-            const deleteButton = document.createElement("button");
-            deleteButton.classList.add("button", "is-danger");
-            deleteButton.innerHTML = trashIconHTML;
-            if (be.active) {
-                    deleteButton.setAttribute("disabled", true);
-            } else {
+            if (!be.active) {
+                const deleteButton = document.createElement("button");
+                deleteButton.classList.add("delete");
                 const r = i; // Save the current value for the closure below.
                 deleteButton.addEventListener("click", () => {
                     be_destroy(r, be.name);
                 });
+                del.appendChild(deleteButton);
             }
-            del.appendChild(deleteButton);
             ++i;
         }
         const section = document.querySelector("#boot-envs-table");
@@ -111,8 +106,7 @@ const handlers = new Map([
             is_be.innerText = be_find(snap.name) ? "yes" : "no";
             const del = row.insertCell();
             const deleteButton = document.createElement("button");
-            deleteButton.classList.add("button", "is-danger");
-            deleteButton.innerHTML = trashIconHTML;
+            deleteButton.classList.add("delete");
             const r = i; // Save the current value for the closure below.
             deleteButton.addEventListener("click", () => {
                 snap_delete(r, snap.name);
